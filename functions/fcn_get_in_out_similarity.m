01934c464961ab59f397936361fe2a528aee50ae
JacobColbyTanner <59493314+JacobColbyTanner@users.noreply.github.com>
2024-01-17 11:23:00 -0500

    Update fcn_get_in_out_similarity.m

--- functions/fcn_get_in_out_similarity.m
+++ functions/fcn_get_in_out_similarity.m
@@ -1,5 +1,27 @@
 function [inout_sim,p] = fcn_get_in_out_similarity(B)
 
+%Function to measure the laterality of a modular partition. Laterality measures how much a module favors one hemisphere over the other.
+%
+%   
+%
+%   Inputs:
+%       B,
+%           the coefficients from the linear regression model that represent the new weights in the AWS connectome
+%       
+%              
+%
+%   Outputs:
+%     
+%       asymmetry,
+%           asymmetry matrix where each value gives the difference between edge(i,j) and edge(j,i)
+%       abs_asymmetry,
+%           absolute asymmetry matrix where each value gives the absolute difference between edge(i,j) and edge(j,i)
+%           
+%
+%   Example:
+%        [B,tspred,corr_pred_obs,MSE, local_error] = fcn_run_sc_regress(sc,ts);
+%        [asymmetry, abs_asymmetry] = fcn_get_asymmetry(B);
+
     id = B == 0;
     B(id) = nan;
     
