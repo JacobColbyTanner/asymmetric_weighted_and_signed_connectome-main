0d04ac056165b5fd95716178c32fa7cc5e7aa48b
JacobColbyTanner <59493314+JacobColbyTanner@users.noreply.github.com>
2024-01-17 11:32:23 -0500

    Update fcn_get_geometric_null.m

--- functions/fcn_get_geometric_null.m
+++ functions/fcn_get_geometric_null.m
@@ -1,5 +1,27 @@
 function b = fcn_get_geometric_null(sc)
 
+
+%
+%
+%   
+%
+%   Inputs:
+%       sc,
+%           weighted structural connectome (e.g. AWS connectome, or fiber density connectome)
+%       
+%              
+%
+%   Outputs:
+%     
+%       b,
+%           null connectivity matrix
+%       
+%           
+%
+%   Example:
+%        b = fcn_get_geometric_null;
+%        [edge_usage,percent_usage] = fcn_get_edge_usage(B)
+
     
     mtrx = sc;
     mask = mtrx ~= 0;
