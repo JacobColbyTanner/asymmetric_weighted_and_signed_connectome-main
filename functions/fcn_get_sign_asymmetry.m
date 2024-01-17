1962f94f722aabfd19d0cb1c67a4db52531ee347
JacobColbyTanner <59493314+JacobColbyTanner@users.noreply.github.com>
2024-01-17 11:27:11 -0500

    Update fcn_get_sign_asymmetry.m

--- functions/fcn_get_sign_asymmetry.m
+++ functions/fcn_get_sign_asymmetry.m
@@ -1,6 +1,29 @@
 function [sign_asym] = fcn_get_sign_asymmetry(B)
 
 
+%
+%
+%   
+%
+%   Inputs:
+%       B,
+%           the coefficients from the linear regression model that represent the new weights in the AWS connectome
+%       
+%              
+%
+%   Outputs:
+%     
+%       sign_asym,
+%           a binary matrix where each 1 indicates where edge(i,j) and edge(j,i) have a different sign (+/-) in the AWS connectome
+%
+%           
+%
+%   Example:
+%        [B,tspred,corr_pred_obs,MSE, local_error] = fcn_run_sc_regress(sc,ts);
+%        [sign_asym] = fcn_get_sign_asymmetry(B)
+
+
+
     pos = B > 0;
     neg = B < 0;
     %get elements for i,j is pos/neg and j,i is neg,pos
