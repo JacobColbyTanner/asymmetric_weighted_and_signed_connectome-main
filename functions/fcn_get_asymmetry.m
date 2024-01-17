69ee4eaff6c7aa29b26c440bcda5bfe97889665f
JacobColbyTanner <59493314+JacobColbyTanner@users.noreply.github.com>
2024-01-17 11:21:08 -0500

    Update fcn_get_asymmetry.m

--- functions/fcn_get_asymmetry.m
+++ functions/fcn_get_asymmetry.m
@@ -1,5 +1,27 @@
 function [asymmetry, abs_asymmetry] = fcn_get_asymmetry(B)
 
+%Function to measure the laterality of a modular partition. Laterality measures how much a module favors one hemisphere over the other.
+%
+%   
+%
+%   Inputs:
+%       B,
+%           modular partition of each node into different communities represented as a list of community labels
+%       
+%              
+%
+%   Outputs:
+%     
+%       asymmetry,
+%           asymmetry matrix where each value gives the difference between edge(i,j) and edge(j,i)
+%       abs_asymmetry,
+%           absolute asymmetry matrix where each value gives the absolute difference between edge(i,j) and edge(j,i)
+%           
+%
+%   Example:
+%      
+%        [asymmetry, abs_asymmetry] = fcn_get_asymmetry(B)
+
 
     asymmetry = B-B';
     abs_asymmetry = abs(B-B');
